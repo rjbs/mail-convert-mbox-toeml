b41d50f2b0aee8015bfcf667e994210716a5ce5c
Ricardo Signes <rjbs@cpan.org>
2008-05-21 12:50:58 +0000

    most recent release

--- test.pl
+++ test.pl
@@ -17,3 +17,20 @@ ok(1); # If we made it this far, we're ok.
 
 #my $MBX=Mail::Convert::Mbox::ToEml->new("D:/mail/PO and Portal", "D:/Download/Entwicklung/Delphi/out1");
 #my $ret=$MBX->CreateEML();
+#my @liste=$MBX->GetMessages();
+#my %h = $MBX->FindMessage("WebPortal");
+#foreach (keys %h)
+#{
+#	print "The key: $_="; 
+#		foreach my $xx (keys %{$h{$_}})  
+#		{
+#			print "$xx=" . %{$h{$_}}->{$xx} . " ";
+#		}
+#	print " \n";
+#}
+#	print " \n";
+#	print " \n";
+#foreach (@liste)
+#{
+#	print "$_\n";
+#}
